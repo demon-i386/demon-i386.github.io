efd1720e6168cea26e559c3eb9788907e917e7f6
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2021-06-18 19:03:36 -0300

    Update iploggerkk.js

--- iploggerkk.js
+++ iploggerkk.js
@@ -1,28 +1,6 @@
 var userData = {
-    "internalIPs": [],
-    "externalIPs": {"ipv4": [], "ipv6": []},
-    "fingerprintHash": '',
-    "userAgent": navigator.userAgent
+    "haha": [],
 }
-
-getIPs(function(ip){
-    //local IPs
-    if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/))
-        userData.internalIPs.push(ip);
-    //IPv6 addresses
-    else if (ip.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/))
-        userData.externalIPs["ipv6"].push(ip);
-    //assume the rest are public IPs
-    else
-        userData.externalIPs["ipv4"].push(ip);
-});
-
-new Fingerprint2().get(function(fingerprint, components){
-  // this will use all available fingerprinting sources
-  userData.fingerprintHash = fingerprint
-
-  // components is an array of all fingerprinting components used
-});
 
 var sendInfo = function(){
     setTimeout(function(){
